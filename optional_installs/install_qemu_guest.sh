d490a9efb462ffd56ddace2ed6e18f57e4f20adb
thamyekh <yek3y.ht@gmail.com>
2024-06-13 16:31:16 +1200

    minor fixes

--- optional_installs/install_qemu_guest.sh
+++ optional_installs/install_qemu_guest.sh
@@ -8,8 +8,6 @@
 # enable shared clipboard between host and qemu guest
 echo 'deb http://ftp.nz.debian.org/debian sid main' | sudo tee -a /etc/apt/sources.list
 sudo apt update && sudo apt -y install spice-vdagent
-# TODO: remove or add: sudo apt -y install xserver-xorg-video-xql
-# enable and start service
 echo 'exec --no-startup-id spice-vdagent' | tee -a $HOME/.config/i3/config
 echo "Done! Please reboot for clipboard share to take effect."
 
