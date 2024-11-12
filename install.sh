d940595e29a998bcedba42bf5b510b25544c9912
Yek Tham <yek3y.ht@gmail.com>
2024-11-12 14:30:52 +1300

    removed naabu for fscan; nabuu isn't portable on linux

--- install.sh
+++ install.sh
@@ -17,10 +17,11 @@ $HOME/.cargo/bin/cargo install rustscan navi
 # wget tools install into /opt
 sudo mkdir -p /opt/lin /opt/web /opt/win
 curl -sL https://api.github.com/repos/jpillora/chisel/releases/latest | jq -r ".assets[].browser_download_url" | grep -e 386 -e amd64 | grep -v darwin | tee /tmp/git_download
-curl -sL https://api.github.com/repos/projectdiscovery/naabu/releases/latest | jq -r ".assets[].browser_download_url" | grep -e 386 -e amd64 | tee -a /tmp/git_download
 curl -sL https://api.github.com/repos/nicocha30/ligolo-ng/releases/latest | jq -r ".assets[].browser_download_url" | grep -e 386 -e amd64 | grep agent | tee -a /tmp/git_download
 cat /tmp/git_download | grep linux | tee /tmp/git_download_lin
 cat /tmp/git_download | grep windows | tee /tmp/git_download_win
+curl -sL https://api.github.com/repos/shadow1ng/fscan/releases/latest | jq -r ".assets[].browser_download_url" | grep -e fscan$ | tee -a /tmp/git_download_lin
+curl -sL https://api.github.com/repos/shadow1ng/fscan/releases/latest | jq -r ".assets[].browser_download_url" | grep -e fscan.exe$ | tee -a /tmp/git_download_win
 elf=$(curl -sL https://api.github.com/repos/ernw/static-toolbox/releases | jq -r ".[].tag_name" | grep nmap | head -n 1)
 curl -sL https://api.github.com/repos/ernw/static-toolbox/releases/tags/$elf | jq -r ".assets[].browser_download_url" | grep _64 | grep tar | tee -a /tmp/git_download_lin
 elf=$(curl -sL https://api.github.com/repos/ernw/static-toolbox/releases | jq -r ".[].tag_name" | grep socat | head -n 1)
