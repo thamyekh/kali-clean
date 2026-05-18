29df3dfd926b6e00080222abb19fd9e023182011
kali <kali@kali.hack.lan>
2026-05-19 09:41:47 +1200

    updated baremetal installation

--- optional_installs/install_baremetal_enhancements/install_baremetal_enhancements.sh
+++ optional_installs/install_baremetal_enhancements/install_baremetal_enhancements.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt -y install mpv pavucontrol remmina gnome-calendar network-manager
+# acpi - needed for displaying battery in i3blocks
+sudo apt -y install acpi mpv pavucontrol remmina gnome-calendar network-manager
 cp ../../optional_cheats/linux.cheat $HOME/.local/share/navi/cheats
 cp config $HOME/.config/i3/config
-sudo cp volume-pipewire /usr/share/i3blocks
-sudo cp datetime /usr/share/i3blocks
+sudo mkdir -p /usr/share/i3blocks
+# see blockets at https://github.com/vivien/i3blocks-contrib for updated scripts
+sudo cp bandwidth config datetime volume-pipewire cpu_usage disk iface memory /usr/share/i3blocks
 cp i3blocks.conf $HOME/.config/i3/i3blocks.conf
 sudo update-locale LANG=en_US.UTF-8
