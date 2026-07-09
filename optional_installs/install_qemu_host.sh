6813440370180b8321930f350c454659be66ccfc
kali <kali@kali.hack.lan>
2026-07-09 13:23:33 +1200

    qemu host dependencies

--- optional_installs/install_qemu_host.sh
+++ optional_installs/install_qemu_host.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt -y install qemu-utils qemu-system-modules-spice virt-manager
+sudo apt -y install qemu-utils qemu-system-modules-spice virt-manager gir1.2-spiceclientgtk-3.0
+sudo usermod -aG libvirt $USER && newgrp libvirt
+
